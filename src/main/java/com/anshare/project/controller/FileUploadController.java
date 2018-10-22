21f4375e9d512f2423b3cf075f14ce1eff4fde5a
BoBoooooo <17746714@qq.com>
2018-10-22 18:34:31 +0800

    加入文件上传下载接口

--- src/main/java/com/anshare/project/controller/FileUploadController.java
+++ src/main/java/com/anshare/project/controller/FileUploadController.java
@@ -7,21 +7,21 @@ import com.anshare.project.model.Affix;
 import com.anshare.project.service.AffixService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.UUID;
-import java.io.File;
+
 /**
  * Created by Anshare on 2018/09/27.
  */
 @RestController
-@RequestMapping("/fileupload")
+@RequestMapping("/file")
 @Api(value = "上传文件", description = "上传文件")
 
 public class FileUploadController {
@@ -29,38 +29,34 @@ public class FileUploadController {
     @Resource
     private AffixService affixService;
 
-    /*
-     * 通过流的方式上传文件
-     * @RequestParam("file") 将name=file控件得到的文件封装成CommonsMultipartFile 对象
-     */
     @ApiOperation(value = "上传文件")
 
-    @RequestMapping("fileUpload")
+    @PostMapping("/Upload")
 
-    public Result Upload(@RequestParam("file") MultipartFile file,@RequestParam("MasterID") String MasterID,
-                  HttpServletRequest request) {
+    public Result Upload(@RequestParam("file") MultipartFile file,
+                         HttpServletRequest request) {
         String savename = UUID.randomUUID().toString();
-
+  String MasterID  =  request.getParameter("MasterID");
         String fileName = file.getOriginalFilename();
-        String fileExtension = "."+FileUtil.getExtensionName(fileName);
+        String fileExtension = "." + FileUtil.getExtensionName(fileName);
         /*System.out.println("fileName-->" + fileName);
         System.out.println("getContentType-->" + contentType);*/
-        String filePath = "/Users/BoBo/AnshareSpringBootAdmin/src/main/UploadFiles";
-        File dest = new File(filePath + savename+fileExtension);
+        String filePath = "/Users/BoBo/AnshareSpringBootAdmin/src/main/UploadFiles/";
+        File dest = new File(filePath + savename + fileExtension);
         try {
             file.transferTo(dest);
         } catch (IOException e) {
         }
-       System.out.println(filePath);
+        System.out.println(filePath);
         try {
 
             Affix temp = new Affix();
             temp.setFilename(fileName);
             temp.setFileextension(fileExtension);
             temp.setMasterid(MasterID);
-            temp.setSavename(savename+fileExtension);
+            temp.setSavename(savename + fileExtension);
 
-            affixService.save(temp,true);
+            affixService.save(temp, true);
             return ResultGenerator.genSuccessResult("上传成功");
 
         } catch (Exception e) {
@@ -70,8 +66,70 @@ public class FileUploadController {
         }
         //返回json
     }
+    @ApiOperation(value = "下载文件")
+
+    @GetMapping("/download")
+    public Result Download(@RequestParam("AffixID") String AffixID, HttpServletResponse response) throws UnsupportedEncodingException {
+
+        if (AffixID != "") {
+            //设置文件路径
+            String filePath = "/Users/BoBo/AnshareSpringBootAdmin/src/main/UploadFiles/";
+
+            Affix temp = affixService.findById(AffixID);
+
+            File file = new File(filePath + temp.getSavename());
+            //File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/octet-stream");// 设置强制下载不打开
+                response.addHeader("Content-Disposition",
+                        "attachment; filename=" +
+                                URLEncoder.encode(temp.getFilename(),"UTF-8")
 
 
+                );
+                response.addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+                response.addHeader("Pragma", "no-cache");
+                response.addHeader("Expires", "0");
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return ResultGenerator.genSuccessResult("下载成功");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        if (bis != null) {
+                            try {
+                                bis.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (fis != null) {
+                            try {
+                                fis.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+            return ResultGenerator.genSuccessResult("下载失败");
+        } else
+            return ResultGenerator.genSuccessResult("下载失败");
+
+
+    }
 
 
 }
